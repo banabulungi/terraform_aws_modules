2c5e67dbf3f19a09fb4a099b1b304934914ad8bc
Biniyam Wolderufael <biniyam.gessese@gmail.com>
2026-05-26 02:20:20 -0700

    refactor: fix S3 bucket module outputs to use new resource name (s3_bucket_name_biniyam->bucket)

--- modules/s3_bucket/outputs.tf
+++ modules/s3_bucket/outputs.tf
@@ -1,11 +1,11 @@
 output "bucket_name" {
   description = "Name of the created S3 bucket."
-  value       = aws_s3_bucket.s3_bucket_name_biniyam.id
+  value       = aws_s3_bucket.bucket.id
 
 }
 
 output "bucket_arn" {
   description = "ARN of the created S3 bucket."
-  value       = aws_s3_bucket.s3_bucket_name_biniyam.arn
+  value       = aws_s3_bucket.bucket.arn
 }
 
